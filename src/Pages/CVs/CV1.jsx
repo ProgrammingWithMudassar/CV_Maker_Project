e529e78afb7a41d323fb23957c8c9f208261e058
Mudassar Hussain <mudassarhus667788@gmail.com>
2023-05-16 00:21:12 +0500

    update code

--- src/Pages/CVs/CV1.jsx
+++ src/Pages/CVs/CV1.jsx
@@ -3,6 +3,10 @@ import { Container, Box, Button, Grid, Typography, Divider } from '@mui/material
 import { Link } from 'react-router-dom'
 import ReplyIcon from '@mui/icons-material/Reply';
 import '../Style.css'
+import CallIcon from '@mui/icons-material/Call';
+import EmailIcon from '@mui/icons-material/Email';
+import HomeIcon from '@mui/icons-material/Home';
+import LinkIcon from '@mui/icons-material/Link';
 
 const CV1 = () => {
 
@@ -16,12 +20,13 @@ const CV1 = () => {
         <Button sx={{ position: 'realtive', left: { xs: "120px", sm: "250px", md: "550px" } }} onClick={goBack}> <ReplyIcon sx={{ ml: -0.5, mt: -0.5 }} /> Back </Button>
       </Box>
       <Container className='CV_container'>
+        {/* Left  */}
         <Grid container spacing={4} >
-          <Grid item xs={4} textAlign='left' backgroundColor='#000' color="#fff" pb={2}>
+          <Grid item xs={4} textAlign='left' backgroundColor='#053D71' color="#fff" pb={2}>
             <Box className="ImgBoxInCv">
-              <Typography variant="body1" color="initial">Image</Typography>
+              <Typography variant="body1" color="initial"></Typography>
             </Box>
-            <Box textAlign='center' mt={2}>
+            <Box textAlign='center' mt={4}>
               <Typography variant="h4" fontWeight={600}>Mudasssar Hussain</Typography>
               <Typography variant="h6" >Job Position</Typography>
             </Box>
@@ -29,22 +34,82 @@ const CV1 = () => {
               variant="fullWidth"
               orientation="horizontal"
               color="#fff"
-              sx={{my:2}}
+              sx={{ my: 2 }}
             />
 
-            <Box textAlign='left' mt={2}>
+            {/* Contact  */}
+            <Box textAlign='left' mt={4}>
               <Typography variant="h5" fontWeight={600}>Contact Me.</Typography>
-              <Typography variant="body1" sx={{ textIndent: { xs: "50px", md: '100px' } }}>sadfga</Typography>
+              <Box display='flex' alignItems='center' mt={1} >
+                <CallIcon color="#fff" sx={{mx:3}}/>
+                <Typography variant="body1" >031246002885</Typography>
+              </Box>
+              <Box display='flex' alignItems='center' mt={1} >
+                <EmailIcon color="#fff" sx={{mx:3}}/>
+                <Typography variant="body1" >www.emal@gmail.com</Typography>
+              </Box>
+              <Box display='flex' alignItems='center' mt={1} >
+                <HomeIcon color="#fff" sx={{mx:3}}/>
+                <Typography variant="body1" >safgsdfh erjerue ,567</Typography>
+              </Box>
+              <Box display='flex' alignItems='center' mt={1} >
+                <LinkIcon color="#fff" sx={{mx:3}}/>
+                <Typography variant="body1" >http://www.emal@gmail.com</Typography>
+              </Box>
+            </Box>
+
+            {/* Skill */}
+            <Box textAlign='left' mt={4}>
+            <Typography variant="h5" fontWeight={600}>Skill's.</Typography>
+              <Box sx={{display:'flex',px:3, justifyContent:'space-between'}}>
+                <Typography variant="body1" >HTML</Typography>
+                <Typography variant="body1" >23%</Typography>
+              </Box>
+              <Box sx={{display:'flex',px:3, justifyContent:'space-between'}}>
+                <Typography variant="body1" >Researching</Typography>
+                <Typography variant="body1" >23%</Typography>
+              </Box>
+              <Box sx={{display:'flex',px:3, justifyContent:'space-between'}}>
+                <Typography variant="body1" >HTML</Typography>
+                <Typography variant="body1" >23%</Typography>
+              </Box>
+              <Box sx={{display:'flex',px:3, justifyContent:'space-between'}}>
+                <Typography variant="body1" >HTML</Typography>
+                <Typography variant="body1" >23%</Typography>
+              </Box>
+              <Box sx={{display:'flex',px:3, justifyContent:'space-between'}}>
+                <Typography variant="body1" >HTML</Typography>
+                <Typography variant="body1" >23%</Typography>
+              </Box>
+            </Box>
+
+            {/* Hobbies */}
+            <Box textAlign='left' mt={4}>
+            <Typography variant="h5" fontWeight={600}>Skill's.</Typography>
+              <Box sx={{display:'flex',px:3, justifyContent:'space-between'}}>
+                <Typography variant="body1" >HTML</Typography>
+                <Typography variant="body1" >23%</Typography>
+              </Box>
+              <Box sx={{display:'flex',px:3, justifyContent:'space-between'}}>
+                <Typography variant="body1" >Researching</Typography>
+                <Typography variant="body1" >23%</Typography>
+              </Box>
+              <Box sx={{display:'flex',px:3, justifyContent:'space-between'}}>
+                <Typography variant="body1" >HTML</Typography>
+                <Typography variant="body1" >23%</Typography>
+              </Box>
             </Box>
 
           </Grid>
+
+          {/* Right  */}
           <Grid item xs={8} textAlign='left'>
-          <Box mt={2}>
+            <Box mt={2}>
               <Typography variant="h5" fontWeight={600}>About.</Typography>
-              <Typography variant="body1" sx={{ textIndent: { xs: "50px"} }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque iaculis,
-               commodo nisl eget, vulputate orci. Praesent condimentum nisl eu facilisis egestas. 
-               Vivamus porttitor lacinia dui sed tincidunt. Integer maximus, leo eu eleifend laoreet, 
-               felis tortor eleifend odio, sit amet suscipit quam ante id justo. Nunc rhoncus erat ac lectus malesuada.</Typography>
+              <Typography variant="body1" sx={{ textIndent: { xs: "50px" } }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut neque iaculis,
+                commodo nisl eget, vulputate orci. Praesent condimentum nisl eu facilisis egestas.
+                Vivamus porttitor lacinia dui sed tincidunt. Integer maximus, leo eu eleifend laoreet,
+                felis tortor eleifend odio, sit amet suscipit quam ante id justo. Nunc rhoncus erat ac lectus malesuada.</Typography>
             </Box>
           </Grid>
         </Grid>
