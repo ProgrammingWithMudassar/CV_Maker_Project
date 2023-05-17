b5325cefd8d61dd9b98839202f1089b6648f56d4
Mudassar Hussain <mudassarhus667788@gmail.com>
2023-05-18 03:34:57 +0500

    update code

--- src/Pages/CV_Form.jsx
+++ src/Pages/CV_Form.jsx
@@ -31,14 +31,14 @@ const EditTemplate = () => {
 
         if (
             Name
-            // && Job_position && Number && Email && Address && About && Hob1 && Hob2 &&
-            // skill1 && skill2 && skill3 &&
-            // skill_exp1 && skill_exp2 && skill_exp3 &&
-            // Language1 && Language2 && Language3 &&
-            // Edu1_start_year && Edu1_end_year && Edu1_degree && Edu1_details &&
-            // Edu2_start_year && Edu2_end_year && Edu2_degree && Edu2_details &&
-            // Exp1_start_year && Exp1_end_year && Exp1_compnay_name && Exp1_details &&
-            // Exp2_start_year && Exp2_end_year && Exp2_compnay_name && Exp2_details
+             && Job_position && Number && Email && Address && About && Hob1 && Hob2 &&
+            skill1 && skill2 && skill3 &&
+            skill_exp1 && skill_exp2 && skill_exp3 &&
+            Language1 && Language2 && Language3 &&
+            Edu1_start_year && Edu1_end_year && Edu1_degree && Edu1_details &&
+            Edu2_start_year && Edu2_end_year && Edu2_degree && Edu2_details &&
+            Exp1_start_year && Exp1_end_year && Exp1_compnay_name && Exp1_details &&
+            Exp2_start_year && Exp2_end_year && Exp2_compnay_name && Exp2_details
 
         ) {
             dispatch(UpdateState(FormData))
@@ -316,7 +316,7 @@ const EditTemplate = () => {
                                                 placeholder='Describe into 150 letters'
                                                 className='textarea_input'
                                                 rows="6" cols="50"
-                                                maxLength={150} defaultValue={DefaultFormData.Edu3_details}
+                                                maxLength={150} defaultValue={DefaultFormData.Exp3_details}
                                                 onChange={(e) => setFormData({ ...FormData, Exp3_details: e.target.value })}
                                             /></ListItem>
                                     </Grid>
